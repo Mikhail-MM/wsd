76099c8e3dd2b96c2da2910d15babbe4bea4929b
Mikhail Maslyuk <leakymee@bu.edu>
2018-09-21 18:14:18 -0400

    Configure automated SMS

--- wsd-server/controllers/mailer.js
+++ wsd-server/controllers/mailer.js
@@ -17,7 +17,12 @@ module.exports.sendContactNotification = async (req, res, next) => {
 		from: ' "World Smiles Dental" <wsdmailernoreply@gmail.com>',
 		to: req.body.email,
 		subject: "Your Appointment Confirmation!",
-		text: `Hello ${req.body.firstName} ${req.body.lastName}, This is an automatic confirmation from World Smiles Dental regarding your correspondence. Your message has been received by our office - We will contact you via your email at ${req.body.email}, or the phone number you gave us: ${req.body.phone}.`	
+		text: `Hello ${req.body.firstName} ${req.body.lastName}, 
+		This is an automatic confirmation from World Smiles Dental regarding your correspondence. Your message has been received by our office - We will contact you regarding your concern via your email at ${req.body.email}, or the phone number you gave us: ${req.body.phone}.
+		
+		World Smiles Dental
+		1417 W. Warner Ave Santa Ana, CA 92704
+		(714)-879-8118`	
 	}
 	transporter.sendMail(mailOptions, (error, info) => {
 		if (error) {
@@ -34,7 +39,13 @@ module.exports.sendAppointmentNote = async (req, res, next) => {
 		from: ' "World Smiles Dental" <wsdmailernoreply@gmail.com>',
 		to: req.body.email,
 		subject: "Your Appointment Confirmation!",
-		text: `Hello, This is an automatic confirmation from World Smiles Dental regarding an upcoming appointment for ${req.body.firstName} ${req.body.lastName}. Your request has been received by our office, and we will be expecting your arrival on ${moment(req.body.startDate).format("MMM Do")}. If this is your first visit with us, please bring your insurance information. We look forward to seeing you! Thank you.`	
+		text: `Hello, 
+		This is an automatic confirmation from World Smiles Dental regarding an upcoming appointment for ${req.body.firstName} ${req.body.lastName}. Your request has been received by our office, and we will be expecting your arrival on ${moment(req.body.startDate).format("MMM Do")}. 
+		If this is your first visit with us, please bring your insurance information. We look forward to seeing you! Thank you.
+
+		World Smiles Dental
+		1417 W. Warner Ave Santa Ana, CA 92704
+		(714)-879-8118`	
 	}
 	transporter.sendMail(mailOptions, (error, info) => {
 		if (error) {
@@ -43,6 +54,21 @@ module.exports.sendAppointmentNote = async (req, res, next) => {
 		console.log('Message sent: %s', info.messageId);
 	});
 
-
+	let smsOptions = {
+		from: ' "World Smiles Dental" <wsdmailernoreply@gmail.com>',
+		to: '9084009318@txt.att.net',
+		subject: "New Appointment",
+		text: `Patient Name: ${req.body.firstName} ${req.body.lastName}
+		Email: ${req.body.email}
+		Phone: ${req.body.phone}
+		Requested Date: ${moment(req.body.startDate).format("MMM Do")}
+		Requested Time: ${req.body.time}`	
+	}
+	transporter.sendMail(mailOptions, (error, info) => {
+		if (error) {
+			return console.log(error); 
+		}
+		console.log('Message sent: %s', info.messageId);
+	});
 	res.json(req.body)
 }
