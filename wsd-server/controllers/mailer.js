bb92ce8174afd8d641b36b9edde0f415be716784
Mikhail Maslyuk <leakymee@bu.edu>
2018-09-21 18:00:22 -0400

    Attempt SMTP fix without nodemailer testacc boilerplate

--- wsd-server/controllers/mailer.js
+++ wsd-server/controllers/mailer.js
@@ -3,30 +3,6 @@ const moment = require('moment')
 
 const authpass = require('../auth_config/auth')
 
-nodemailer.createTestAccount((err, account) => {
-	let transporter = nodemailer.createTransport({
-		service: 'gmail',
-		host: 'smtp.gmail.com',
-		auth: {
-			user: "wsdmailernoreply@gmail.com",
-			pass: authpass,
-		},
-	});
-	let mailOptions = {
-		from: ' "World Smiles Dental" <wsdmailernoreply@gmail.com>',
-		to: 'mike.maslyuk@yahoo.com',
-		subject: "Your Appointment Confirmation!",
-		text: "Hello. We're mailing you to confirm your appointment. Please respond!",	
-	}
-	transporter.sendMail(mailOptions, (error, info) => {
-		if (error) {
-			return console.log(error); 
-		}
-		console.log('Message sent: %s', info.messageId);
-
-	});
-});
-
 let transporter = nodemailer.createTransport({
 	service: 'gmail',
 	host: 'smtp.gmail.com',
